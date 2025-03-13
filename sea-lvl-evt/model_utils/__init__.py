afa4143b7bd561db7c1340828aba24d2c69f0043
Seuri88 <56756866+Seuri88@users.noreply.github.com>
2025-03-13 18:08:11 +0100

    Update __init__.py

--- sea-lvl-evt/model_utils/__init__.py
+++ sea-lvl-evt/model_utils/__init__.py
@@ -1,6 +1,6 @@
-"""Model setup.""""
+"""Model setup."""
 
-from Baseline import Basline
-from CovarianceSphericalMetric import (Matern32Chordal, Matern52Chordal, ExponentialChordal)
-from HilbertSpaceLatLon import HSGP_LatLon, lonlat2xyz, calculate_Kapprox, calculate_K, approx_hsgp_hyperparams_xyz
-from ModelSetup import model_setup
+from sea_lvl_evt.model_utils.Baseline import Baseline
+from sea_lvl_evt.model_utils.CovarianceSphericalMetric import (Matern32Chordal, Matern52Chordal, ExponentialChordal)
+from sea_lvl_evt.model_utils.HilbertSpaceLatLon import HSGP_LatLon, lonlat2xyz, calculate_Kapprox, calculate_K, approx_hsgp_hyperparams_xyz
+from sea_lvl_evt.model_utils.ModelSetup import model_setup
