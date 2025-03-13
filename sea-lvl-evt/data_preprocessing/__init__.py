6a0a334a21571cb30a481c8458b18e8d388b046d
Seuri88 <56756866+Seuri88@users.noreply.github.com>
2025-03-13 18:09:03 +0100

    Update __init__.py

--- sea-lvl-evt/data_preprocessing/__init__.py
+++ sea-lvl-evt/data_preprocessing/__init__.py
@@ -1,5 +1,5 @@
 """Data preprocessing."""
 
-from CleanGeslaBM import (swe_data, sealvl_cleandata_and_lat_lon, detrend_clean_sealvl_latlon, year_maxima,BlockMaximaRawTsData, BlockMaxima)
-from Cleaning import drop_cells, zero_intrpl_drop, no_flags, all_clean
-from LoadingGesla import GeslaDataset
+from sea_lvl_evt.data_preprocessing.CleanGeslaBM import (swe_data, sealvl_cleandata_and_lat_lon, detrend_clean_sealvl_latlon, year_maxima,BlockMaximaRawTsData, BlockMaxima)
+from sea_lvl_evt.data_preprocessing.Cleaning import drop_cells, zero_intrpl_drop, no_flags, all_clean
+from sea_lvl_evt.data_preprocessing.LoadingGesla import GeslaDataset
